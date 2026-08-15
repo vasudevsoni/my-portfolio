5dfe432fb244e8f1bcfee118fe20077f72c19f93
Vasudev Soni <vasudevsoni2001@gmail.com>
2026-08-15 13:31:42 +0530

    new build and deployed

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/my-portfolio/",
   plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
